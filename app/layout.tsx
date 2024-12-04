0a3be5ce6534cc225a52c35e2c7b1a21152952f3
sebasydoc99 <sebasydoc@gmail.com>
2024-12-04 20:12:57 +0100

    chore: add theme and default configuration to the project.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,13 @@
 import type {Metadata} from "next";
-import localFont from "next/font/local";
+import { JetBrains_Mono } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
+import {Header} from "@/components/Header";
 
-const geistSans = localFont({
-    src: "./fonts/GeistVF.woff",
-    variable: "--font-geist-sans",
-    weight: "100 900",
-});
-const geistMono = localFont({
-    src: "./fonts/GeistMonoVF.woff",
-    variable: "--font-geist-mono",
-    weight: "100 900",
+const jetbrainsMono = JetBrains_Mono({
+    subsets: ["latin"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+    variable: "--font-jetbrainsMono",
 });
 
 export const metadata: Metadata = {
@@ -24,10 +21,18 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
-        </body>
+        <html lang="en" suppressHydrationWarning>
+            <body className={jetbrainsMono.variable}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="dark"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    <Header />
+                    {children}
+                </ThemeProvider>
+            </body>
         </html>
     );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,40 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    darkMode: ["class"],
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        screens: {
+            sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+        },
+        fontFamily: {
+            primary: 'var(--font-jetbrainsMono)',
+        },
+        extend: {
+            colors: {
+                background: {
+                    DEFAULT: '#ffffff',
+                    dark: '#1c1c22',
+                },
+                primary: '#1c1c22',
+                accent: {
+                    DEFAULT: '#00ff99',
+                    hover: "#00e187",
+                }
+            },
+            borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)'
+            }
+        }
     },
-  },
-  plugins: [],
+    plugins: [require("tailwindcss-animate")],
 } satisfies Config;
